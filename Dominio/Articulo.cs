5af193e5ad9145d7e39efeb94b9f5a73a9a27f02
unknown <Jinbyos@Gmail.com>
2024-01-22 19:19:03 -0300

    Creo Libreria Dominio con definicion de clases y sus propiedades

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        public int Id {  get; set; }
+        public string Codigo {  get; set; }
+        public string Nombre {  get; set; }
+        public string Descripcion {  get; set; }
+        public Marca Marca {  get; set; }
+        public Categoria Categoria {  get; set; }
+        public string ImagenUrl {  get; set; }
+        public float Precio {  get; set; }
+    }
+}
---
+++ Dominio/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email {  get; set; }
+        public string Pass {  get; set; }
+        public string Nombre {  get; set; }
+        public string Apellido {  get; set; }
+        public string UrlImagenPerfil { get; set; }
+        //1 es Admin
+        public bool Admin { get; set; }
+
+    }
+}
