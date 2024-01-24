f6f4455a43345b37f6ba7ecf417dd43715444926
unknown <Jinbyos@Gmail.com>
2024-01-24 16:44:57 -0300

    Implementacion de metodo 'listar' en 'CategoriaNegocio'

--- Negocio/CategoriaNegocio.cs
+++ Negocio/CategoriaNegocio.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dominio;
 
 namespace Negocio
 {
     public class CategoriaNegocio
     {
+        public List<Categoria> listar()
+        {
+            AccesoDatos datos = new AccesoDatos();
+            List<Categoria> lista = new List<Categoria>();
+            try
+            {
+                datos.setearConsulta("SELECT Id, Descripcion FROM CATEGORIAS");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Categoria categoria = new Categoria();
+                    categoria.Id = (int)datos.Lector["Id"];
+                    categoria.Descripcion = (string)datos.Lector["Descripcion"];
+
+                    lista.Add(categoria);
+                }
+
+                return lista;
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+            return lista;
+        }
     }
 }
