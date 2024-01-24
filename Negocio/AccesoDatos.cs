e250787f925ef33d7aaec46e1bac820a3fecb44c
unknown <Jinbyos@Gmail.com>
2024-01-24 15:36:24 -0300

    Agregada biblioteca de clases 'Negocio' que contiene clases relacionadas con el dominio. Completada la implementación de 'AccesoDatos', encargada de la conexión a la base de datos SQL.

---
+++ Negocio/AccesoDatos.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+
+namespace Negocio
+{
+    internal class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        public AccesoDatos()
+        {
+            //Conexion centralizada
+            conexion = new SqlConnection(ConfigurationManager.AppSettings["cadenaConexion"]);
+            comando = new SqlCommand();
+        }
+
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+
+        public void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+
+        public void setearParametros(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+
+        //Retorna el ID del nuevo objeto creado despues de ejecutar la consulta, usado para mantener la sesion abierta.
+        public int ejecutarAccionScalar()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                return int.Parse(comando.ExecuteScalar().ToString());
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+
+        public void cerrarConexion()
+        {
+            if(lector != null)
+            {
+                lector.Close();
+                conexion.Close();
+            }
+        }
+    }
+}
