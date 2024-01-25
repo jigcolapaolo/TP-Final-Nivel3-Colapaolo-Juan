ece6c60539fd1e2ead4036debf0ef02853811e98
unknown <Jinbyos@Gmail.com>
2024-01-24 23:58:39 -0300

    Implementacion de metodo 'Login' en 'UserNegocio'

--- Negocio/UserNegocio.cs
+++ Negocio/UserNegocio.cs
@@ -3,11 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dominio;
 
 namespace Negocio
 {
     public class UserNegocio
     {
+        public bool Login(User usuario)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("SELECT Id, email, pass, nombre, apellido, urlImagenPerfil, admin FROM USERS WHERE email = @email AND pass = @pass");
+                datos.setearParametros("@email", usuario.Email);
+                datos.setearParametros("@pass", usuario.Pass);
+                datos.ejecutarLectura();
 
+                while (datos.Lector.Read())
+                {
+                    usuario.Id = (int)datos.Lector["Id"];
+                    usuario.Nombre = (string)datos.Lector["nombre"];
+                    usuario.Apellido = (string)datos.Lector["apellido"];
+                    usuario.UrlImagenPerfil = (string)datos.Lector["urlImagenPerfil"];
+                    usuario.Admin = (bool)datos.Lector["admin"];
+
+                    return true;
+                }
+
+                return false;
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
     }
 }
